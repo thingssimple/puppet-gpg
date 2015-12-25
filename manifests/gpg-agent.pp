d6b691e71bab429efbf424e7fd80b14bd67caaea
Michael Avila <me@michaelavila.com>
2015-12-25 13:56:01 -0800

    Allow gpg-agent to be installed and configured

---
+++ manifests/gpg-agent.pp
@@ -0,0 +1,19 @@
+class gpg::gpg-agent {
+  include homebrew
+
+  package { "gpg-agent":
+    ensure => present
+  }
+
+  $home = "/Users/${::boxen_user}"
+
+  file_line { "Use gpg-agent":
+    path => "${home}/.gnupg/gpg.conf",
+    line => 'use-agent'
+  }
+
+  file_line { "Use gpg-agent daemon":
+    path => "${home}/.zshrc",
+    line => 'eval "$(gpg-agent --daemon)"'
+  }
+}
